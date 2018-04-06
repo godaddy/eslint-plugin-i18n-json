b79db401fd4f9e2c975914b3ec6d223edcfef253
Gustav Føsker <tvarsis@hotmail.com>
2018-04-06 08:25:36 +0200

    moved diff options outside of function

--- src/util/compare-translations-structure.js
+++ src/util/compare-translations-structure.js
@@ -1,6 +1,10 @@
 const set = require('lodash.set');
 const diff = require('jest-diff');
 const deepForOwn = require('./deep-for-own');
+const DIFF_OPTIONS = {
+  expand: false,
+  contextLines: 1
+};
 
 // we don't care what the actual values are.
 // lodash.set will automatically convert a previous string value
@@ -9,17 +13,13 @@ const deepForOwn = require('./deep-for-own');
 const compareTranslationsStructure = (translationsA, translationsB) => {
   const augmentedTranslationsA = {};
   const augmentedTranslationsB = {};
-  const options = {
-    expand: false,
-    contextLines: 1
-  };
   deepForOwn(translationsA, (value, key, path) => {
     set(augmentedTranslationsA, path, 'Message<String>');
   });
   deepForOwn(translationsB, (value, key, path) => {
     set(augmentedTranslationsB, path, 'Message<String>');
   });
-  return diff(augmentedTranslationsA, augmentedTranslationsB, options);
+  return diff(augmentedTranslationsA, augmentedTranslationsB, DIFF_OPTIONS);
 };
 
 module.exports = compareTranslationsStructure;
