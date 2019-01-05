53153a0064c3c7be3ca7155fdc21e7e66fc5c6e4
Mayank <m.jethva01@gmail.com>
2019-01-05 17:17:00 -0600

    edits

--- src/util/require-no-cache.js
+++ src/util/require-no-cache.js
@@ -2,8 +2,6 @@
 // This forces the file to be read from disk again.
 // e.g) webpack dev server eslint loader support
 const requireNoCache = (path) => {
-  process.stdout.write('require-no-cache' + path);
-  throw new Error('failure');
   delete require.cache[path];
   return require(path);
 };
