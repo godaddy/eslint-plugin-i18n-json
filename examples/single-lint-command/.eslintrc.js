59e2f2a30c091a508d571930beb871a2641f8e80
Mayank Jethva <m.jethva01@gmail.com>
2019-11-01 15:52:54 -0700

    example test case

---
+++ examples/single-lint-command/.eslintrc.js
@@ -0,0 +1,6 @@
+const path = require('path');
+
+module.exports = {
+  root: true, // since this example folder is embedded into the project. just ignore this.
+  extends: ['airbnb-base', 'plugin:i18n-json/recommended']
+};
---
+++ examples/single-lint-command/src/index.js
@@ -0,0 +1,8 @@
+const functionOne = () => 1;
+
+const functionTwo = () => 2;
+
+module.exports = {
+  functionOne,
+  functionTwo,
+};
